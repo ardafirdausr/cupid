11cafbaf80738e78fdef9dfcb6659aa9376fb380
Arda Firdaus Ramadhan <ardafirdausr@gmail.com>
2024-06-23 14:23:39 +0700

    Merged in feat/enchancement

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module com.ardafirdausr.cupid
 go 1.21.5
 
 require (
+	github.com/go-faker/faker/v4 v4.4.1
 	github.com/go-playground/locales v0.14.1
 	github.com/go-playground/universal-translator v0.18.1
 	github.com/go-playground/validator/v10 v10.20.0
@@ -53,9 +54,9 @@ require (
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- internal/repository/mongo/seed/user.go
+++ internal/repository/mongo/seed/user.go
@@ -2,9 +2,14 @@ package seed
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"com.ardafirdausr.cupid/internal/entity"
 	"com.ardafirdausr.cupid/internal/pkg/logger"
+	"github.com/go-faker/faker/v4"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -20,6 +25,41 @@ func SetupUserCollection(ctx context.Context, database *mongo.Database) error {
 	if err != nil {
 		logger.Log.Error().Err(err).Msg("failed to create unique index to email")
 		return err
+	}
+
+	return nil
+}
+
+func GenerateDummyUsers(ctx context.Context, database *mongo.Database, userCount int) error {
+	batchSize := 200
+	users := make([]interface{}, 0, batchSize)
+	for i := 0; i < userCount; i++ {
+		user := entity.User{}
+		user.SetPassword("Rahasia123")
+		user.ID = primitive.NewObjectID().Hex()
+		user.Email = faker.Email()
+		user.Bio = faker.Sentence()
+		user.BirthDate, _ = time.Parse(time.DateOnly, faker.Date())
+
+		user.Gender = entity.UserGenderMale
+		user.Name = faker.FirstNameMale() + " " + faker.LastName()
+		if i%2 == 0 {
+			user.Gender = entity.UserGenderFemale
+			user.Name = faker.FirstNameFemale() + " " + faker.LastName()
+		}
+
+		if i > 0 && i%batchSize == 0 || i == userCount-1 {
+			logger.Log.Info().Msg(fmt.Sprintf("inserting %d users", len(users)))
+			if _, err := database.Collection("users").InsertMany(ctx, users); err != nil {
+				logger.Log.Error().Err(err).Msg("failed to insert users")
+				return err
+			}
+
+			users = users[:0]
+		} else {
+			users = append(users, user)
+		}
+
 	}
 
 	return nil
